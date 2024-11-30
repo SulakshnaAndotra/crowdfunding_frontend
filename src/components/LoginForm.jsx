c7f7fd6d9f2e1bb2905b2f7889b0d6f22cb94546
Hardev Singh <sulakshna.andotra@gmail.com>
2024-11-30 12:20:31 +0800

    change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
+import z from "zod";
 import postLogin from "../api/post-login.js";
+
+const loginSchema = z.object({
+    username: z.string().min(1, { message: "Username must not be empty" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" }),
+  });
 
 function LoginForm() {
     const [credentials, setCredentials] = useState({
@@ -13,27 +21,33 @@ function LoginForm() {
 
     const handleChange = (event) => {
         const { id, value } = event.target;
-        setCredentials((prevCredentials) => ({
+        setCredentials((prevCredentials) => {
+          return {
             ...prevCredentials,
             [id]: value,
-        }));
-    };
+          };
+        });
+      };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        if (credentials.username && credentials.password) {
-            postLogin(
-                credentials.username,
-                credentials.password, 
-                credentials.firstname,
-                credentials.lastname,
-                credentials.email
-            ).then((response) => {
-                window.localStorage.setItem("token", response.token);
-            });
+        const result = loginSchema.safeParse(credentials);
+        if (!result.success) {
+          const error = result.error.errors?.[0];
+          if (error) {
+            alert(error.message);
+          }
+          return;
+        } else {
+          postLogin(result.data.username, result.data.password).then((response) => {
+            window.localStorage.setItem("token", response.token);
+            navigate("/");
+          });
         }
-    };
+      };
+    
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
